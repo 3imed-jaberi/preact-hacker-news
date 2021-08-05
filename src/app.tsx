b1b1558056d98a327c012e6cbd0d7cb9d03e48b8
3imed-jaberi <imed_jebari@hotmail.fr>
2021-08-05 02:22:04 +0200

    add app component --main-component 🚀 ..

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,42 @@
-import { Logo } from './logo'
+import { useEffect, useState } from 'preact/hooks';
+import NewsItem from './components/NewsItem';
+import { getNewStories } from './services';
 
-export function App() {
+function App () {
+  const [allNewsRefs, setAllNewsRefs] = useState<number[]>([])
+  const [news, setNews] = useState<number[]>([])
+
+  const onScroll = () => {
+    // isPassMaxOffsetHeightValue --condition
+    if (window.innerHeight + window.pageYOffset + 200 >= document.body.offsetHeight) loadMoreNews();
+  };
+
+  const loadMoreNews = (size = 10) => {
+    const nextNews = allNewsRefs.splice(0, size)
+    setNews(news => [...news, ...nextNews])
+  };
+
+  useEffect(() => {   
+    (async () => {
+      // fetch all news
+      const _allNewsRefs = await getNewStories();
+      // save all news to state
+      setAllNewsRefs(_allNewsRefs!)
+      // dispaly only 20 news
+      loadMoreNews(20);
+      // quick way to handle the scroll event to display more 10 news.
+      window.addEventListener('scroll', onScroll);
+    })()
+  }, [])
+
   return (
-    <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
-    </>
-  )
+    <div className="container">
+      <header>Hacker News</header>
+      <>
+        { news.map((item, index) => <NewsItem key={`${item}#${index}`} id={item} />) }
+      </>
+    </div>
+  );
 }
+
+export default App
